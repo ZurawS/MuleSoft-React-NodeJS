c98e1e7009fbf6ce6ed385db3906297cbb5dc9f9
PawZet <pawelzurawski898@gmail.com>
2021-12-25 21:09:35 +0100

    done

--- startup/routes.js
+++ startup/routes.js
@@ -10,15 +10,7 @@ module.exports = function (app) {
       extended: true,
     })
   );
-
   app.use(cors());
-  app.use(express.json());
-  app.use(
-    express.urlencoded({
-      extended: true,
-    })
-  );
-  app.use(express.urlencoded({ extended: true }));
   app.use(express.static("public"));
   app.use("/api/courses", courses);
   app.use(error);
